91a65a634f2c40a6c7eff9479f6408da92fd8380
Ketchup288 <arsenko525@gmail.com>
2020-12-17 13:15:22 +0200

    First file

--- lab_5_1.cpp
+++ lab_5_1.cpp
@@ -12,15 +12,15 @@ int main()
 	cout << "s = "; cin >> s;
 	cout << "t = "; cin >> t; 
 
-	double sum = h(1, s + t) + h(s, s - t) - h(t, sqrt(pow(s, 2) + pow(t, 2)));
+	double answ = sum(1, s + t) + h(s, s - t) - h(t, sqrt(pow(s, 2) + pow(t, 2)));
 
-	cout << "sum = " << sum << endl;
+	cout << "answ = " << sum << endl;
 
 	return 0;
 
 }
 
-double h(const double x, const double y)
+double sum(const double x, const double y)
 {
 	return (pow(x, 2) + pow(y, 2))/ (1 + pow(x, 2) + pow(x, 2) * pow(x, 2));
 }
